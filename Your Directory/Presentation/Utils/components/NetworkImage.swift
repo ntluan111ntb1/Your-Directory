ee5d667c1b912de8f76edae4c6deb04e3666f280
ntluan111ntb1 <ntluan111ntb@gmail.com>
2024-08-06 14:34:10 +0700

    fix: fix NetWorkImage

--- Your Directory/Presentation/Utils/components/NetworkImage.swift
+++ Your Directory/Presentation/Utils/components/NetworkImage.swift
@@ -6,21 +6,44 @@
 //
 
 import SwiftUI
+import Combine
+
+class ImageLoader: ObservableObject {
+    @Published var image: UIImage?
+    private var cancellable: AnyCancellable?
+
+    func loadImage(from url: URL) {
+        cancellable = URLSession.shared.dataTaskPublisher(for: url)
+            .map { UIImage(data: $0.data) }
+            .replaceError(with: nil)
+            .receive(on: DispatchQueue.main)
+            .assign(to: \.image, on: self)
+    }
+
+    deinit {
+        cancellable?.cancel()
+    }
+}
 
 struct NetworkImage: View {
-  let url: URL?
-
-  var body: some View {
-    if let url = url,
-       let data = try? Data(contentsOf: url),
-       let uiImage = UIImage(data: data) {
-      Image(uiImage: uiImage)
-        .resizable()
-        .aspectRatio(contentMode: .fit)
-    } else {
-      Image(systemName: "person.circle.fill")
-        .resizable()
-        .aspectRatio(contentMode: .fit)
+    let url: URL?
+    @StateObject private var loader = ImageLoader()
+    var body: some View {
+        Group {
+            if let image = loader.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            } else {
+                Image(systemName: "person.circle.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+        }
+        .onAppear {
+            if let url = url {
+                loader.loadImage(from: url)
+            }
+        }
     }
-  }
 }
